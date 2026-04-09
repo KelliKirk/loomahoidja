b822561248ca8b342a6a18501471b03da4aa9717
margitviigi <margit.viigi@voco.ee>
2026-04-09 14:23:00 +0300

    Auth integrated

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
@@ -57,5 +57,4 @@ module.exports = {
     register,
     login
 }
-
 
